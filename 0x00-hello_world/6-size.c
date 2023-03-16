23cff3787b2d697eafb21cc04fa743d70fe9ce74
keffakevin <kabagambe400@gmail.com>
2023-03-16 23:10:24 +0200

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - This is a prograam print a line with puts function
+*main - This is a prograam print a line with puts function
 *Return: 0 (Success)
 */
 int main(void)
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float e;
-printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof(a)); 
-printf("Size of int: %zu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of int: %zu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof (a)); 
+printf("Size of int: %zu byte(s)\n", (unsigned long)sizeof (b));
+printf("Size of int: %zu byte(s)\n", (unsigned long)sizeof (c));
+printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof (d));
+printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof (d));
 return (0);
 }
