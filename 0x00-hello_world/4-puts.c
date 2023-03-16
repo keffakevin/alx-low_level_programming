34048f2ab420c9de34da92cabc698aeb0c240a70
keffakevin <kabagambe400@gmail.com>
2023-03-16 19:58:14 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main -This is a prograam main function
+ * main - This is a prograam main function
  * Return: 0 (Success)
  */
- int main (void){
- 
-puts("\"Programming is like building a multilingual puzzle");
+ int main (void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
- 
- }
+  }
