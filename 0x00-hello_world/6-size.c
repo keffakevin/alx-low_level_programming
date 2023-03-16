f144e4b83ceaad6e6b1c2a407c8f7505e81ce813
keffakevin <kabagambe400@gmail.com>
2023-03-17 00:04:05 +0200

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,17 @@
  */
 int main(void)
 {
+
 char a;
 int b;
 long int c;
 long long int d;
 float e;
+
 prtintf("Size of Charactor: %lu byte(s)\n", (unsigned long)sizeof(a));
-prtintf("Size of Charactor: %lu byte(s)\n", (unsigned long)sizeof(b));
-prtintf("Size of Charactor: %lu byte(s)\n", (unsigned long)sizeof(c));
-prtintf("Size of Charactor: %lu byte(s)\n", (unsigned long)sizeof(d));
-prtintf("Size of Charactor: %lu byte(s)\n", (unsigned long)sizeof(e));
+prtintf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+prtintf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+prtintf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+prtintf("Size of Float: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
