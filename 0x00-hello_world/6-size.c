350d6bd48c179ea57accf9c86788e9dab64d9bab
keffakevin <kabagambe400@gmail.com>
2023-03-17 00:16:24 +0200

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,17 @@
 #include<stdio.h>
+
+
 /**
- * main -This is a program that print a line with print function
- * Return: 0 (Success)
+ * main - This is a program that print a line with print function
+ * Return:0.)
  */
 int main(void)
 {
 
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-
-prtintf("Size of Charactor: %lu byte(s)\n", (unsigned long)sizeof(a));
-prtintf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-prtintf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-prtintf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-prtintf("Size of Float: %lu byte(s)\n", (unsigned long)sizeof(e));
+prtintf("Size of Char: %zu byte(s)\n", (unsigned long)sizeof(char));
+prtintf("Size of int: %zu byte(s)\n", (unsigned long)sizeof(int));
+prtintf("Size of long int: %zu byte(s)\n", (unsigned long)sizeof(long int));
+prtintf("Size of long long int: %zu byte(s)\n", (unsigned long)sizeof(long long int));
+prtintf("Size of Float: %zu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
