1520af411ec3f9d1dad63ce8a2720a962d306fd4
keffakevin <kabagambe400@gmail.com>
2023-03-18 17:50:14 +0200

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -main function to print single numbers
+ * Return:0 (Success)
+ */
+int main(void)
+{
+int n;
+/* your code goes there */
+for (n =  0; n < 10; n++)
+	printf("%d", n);
+printf("\n");
+return (0);
+}
