494e8be5d86141e279634873829eaf2a3f667903
keffakevin <kabagambe400@gmail.com>
2023-03-18 14:52:17 +0200

    When I was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function to print alphabet without letter q and e
+ * Return: (0)
+ */
+int main(void)
+{
+char i;
+/* your code goes there */
+for (i = 'a' ; i <= 'z' ; i++)
+	if (i != 'q' && i != 'e')
+		putchar(i);
+return (0);
+}
