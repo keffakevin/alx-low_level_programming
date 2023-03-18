5c521a3dba95d2c1c55e65fbcfd96f0cb4c055ea
keffakevin <kabagambe400@gmail.com>
2023-03-18 18:12:24 +0200

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -main that print alphabetic reverse
+ * Return:0 (Success)
+ */
+int main(void)
+{
+int n;
+/* your code goes there */
+for (n = 'z' ; n >= 'a' ; n--)
+putchar('\n');
+return (0);
+}
