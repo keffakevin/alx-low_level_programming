4ed5fbfa7e3cf0b810950b8ba668cbd3b613e5b3
keffakevin <kabagambe400@gmail.com>
2023-03-18 18:52:18 +0200

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -main function that display hexagonal numbers
+ * Return:0 (Success)
+ */
+int main(void)
+{
+int n;
+char j;
+for (n = 0; n < 10; n++)
+	putchar(n + '0');
+for (j = 'a'; j <= 'f'; j++)
+	putchar(j);
+putchar('\n');
+/* your code goes there */
+return (0);
+}
