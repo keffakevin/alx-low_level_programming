35d6f3c03beed7696072fb3e39fa8332b53d294d
keffakevin <kabagambe400@gmail.com>
2023-03-18 19:32:36 +0200

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -main function that display numbers
+ * Return:0 (Success)
+ */
+int main(void)
+{
+int n;
+/* your code goes there */
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+}
+return (0);
+putchar('\n');
+}
