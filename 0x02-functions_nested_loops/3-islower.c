c9250fc7c5d899986eed11e5f5aeed23eb604d5e
keffakevin <kabagambe400@gmail.com>
2023-03-22 06:41:22 +0200

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* _islower  program to print lower case and retun one or otherwise
-* Return:0 (Success)
+*_islower -function that print lower case
+*c paratmeter to be printed
+* Return:1 is there is a upper case and 0 otherwise
 */
 int _islower(int c)
 {
-	if (c>= 97 && c <= 122)
-		return (1);
-	else 
-		return (0);
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
 }
