5be3a603e8d34d8ae2cde936c053a3e3667bb483
keffakevin <kabagambe400@gmail.com>
2023-03-22 10:40:19 +0200

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+/**
+ * _isalpha - function that print alahabetic
+ * @c:parameter description
+ * Return:1 and return 0 otherwise
+ */
 int _isalpha(int c)
 {
-if (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
-	_putchar
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	return (1);
+else
+	return (0);
 
 }
