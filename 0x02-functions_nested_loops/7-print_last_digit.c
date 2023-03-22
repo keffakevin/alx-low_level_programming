6e026b32bd5298ede5da542baedd29de8309bbbe
keffakevin <kabagambe400@gmail.com>
2023-03-22 23:01:07 +0200

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * print_last_digit - function that print alahabetic
+ * @a:parameter description
+ * Return:1 and return 0 otherwise
+ */
+int print_last_digit(int a)
+{
+int k;
+k = a % 10;
+if (a < 0)
+k = -k;
+return (k);
+}
