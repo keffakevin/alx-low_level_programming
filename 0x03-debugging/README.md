e987ad7507b577ffe1a55f8b3e5601f1dcd483b3
keffakevin <kabagambe400@gmail.com>
2023-03-23 19:09:35 +0200

    Multiple mains

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-0x03. C - Debuggin
+0x03. C - Debugging
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,17 +1,15 @@
-#include "main.h"
+#include"main.h"
 /**
- * positive_or_negative - function that print positive or negative
+ * positive_or_negative - function that print alahabetic
  * @i:parameter description
- * Return:1 and return -1 otherwise
+ * Return:1 and return 0 otherwise
  */
 void positive_or_negative(int i)
 {
-
-	if (i < 0)
-	printf("%d is Negative", i);
+	if (i > 0)
+	printf("%d is positive\n", i);
+	else if (i < 0)
+	printf("%d is negative\n", i);
 	else
-	{
-	printf("%d possitive", i);
-	}
-	printf("\n");
+	printf("%d is zero\n", i);
 }
