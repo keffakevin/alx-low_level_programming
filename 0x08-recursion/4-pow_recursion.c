b694b8667a747c9f1e34c5240d129aa0431abbdf
keffakevin <kabagambe400@gmail.com>
2023-04-06 01:14:51 +0200

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * _pow_recursion - function that returns the value of x to power of y
+ * @b: string to print
+ * @a: string to print
  * Return:0
  */
 int _pow_recursion(int b, int a)
 {
-	if (a<0)
+	if (a < 0)
 	{
-	return -1;
+	return (-1);
 	}
 		else if (a != 0)
 		return (b * _pow_recursion(b, a - 1));
 		else
-		return 1;
+		return (1);
 }
