3232f471767a2a9e4ed48ee6ba325d5847ee4368
keffakevin <kabagambe400@gmail.com>
2023-04-05 03:26:15 +0200

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int sum3 = 0, sum7 = 0;
+	int i;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum3 += i;
+		}
+		if (i % 5 == 0)
+		{
+			sum7 += i;
+		}
+	}
+	printf("%d\n", sum3 + sum7);
+	return 0;
+}
