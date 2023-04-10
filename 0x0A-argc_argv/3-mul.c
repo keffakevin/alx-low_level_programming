d9c2ed95b45a43d2e571d547d51afed8f9a0c0bb
keffakevin <kabagambe400@gmail.com>
2023-04-10 21:19:40 +0200

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- function that print alahabetic
+ * @argc:parameter description
+ * @argv:parameter description
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+int a, b, sum;
+if (argc != 3)
+{
+printf("Error\n");
+return (-1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+sum = a * b;
+printf("%d\n", sum);
+return (0);
+}
