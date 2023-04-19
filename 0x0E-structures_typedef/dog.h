7594a6249b8295872b3364c090cbb66572f23601
keffakevin <kabagambe400@gmail.com>
2023-04-19 04:22:12 +0200

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - Defines a dog's attributes
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the dog's owner
+ *
+ * Description: This struct defines a dog's attributes, including its name,
+ * age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* MAIN_H */
