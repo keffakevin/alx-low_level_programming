ad6c3079ce194c03fd3d7fccfe0d019e419ccf3f
keffakevin <kabagambe400@gmail.com>
2023-04-19 03:54:42 +0200

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 /**
  * ABS - Computes the absolute value of a number x
  * @x: the number to compute the absolute value of
@@ -5,3 +7,4 @@
  * Return: the absolute value of x
  */
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif /* MAIN_H */
