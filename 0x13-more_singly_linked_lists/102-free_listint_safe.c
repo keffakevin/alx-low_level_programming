2751491dccc54b1f7db5d1fd6edf964117b56d29
keffakevin <kabagambekevin@gmai.com>
2023-04-29 01:56:01 +0200

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,7 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * free_listint_safe - Frees a listint_t list safely
  * @h: pointer to a pointer to the head of the list
