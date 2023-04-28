6a22bc74e44eb8f21b8caead63d2179eb1398393
keffakevin <kabagambe.kevin05@gmail.com>
2023-04-28 21:38:40 +0300

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	print_before_main();
 	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 	return (0);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,23 +10,24 @@
  *
  * Return: The address of the new element, or NULL if it failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *last_node;
 	char *str_copy;
 
-	if (!head || !str)  /* Check for invalid arguments */
+	if (!head || !str)
+	{
+	}
+	new_node = malloc(sizeof(*new_node));
+	if (!new_node)
+	{
 	return (NULL);
+	}
 
-	new_node = malloc(sizeof(list_t));  /* Allocate memory for the new node */
-	if (!new_node)  /* Check for allocation failure */
-	return (NULL);
-
-	str_copy = strdup(str);  /* Duplicate the string */
+	str_copy = strdup(str);
 	if (!str_copy)
-	{  /* Check for allocation failure */
+	{
 	free(new_node);
 	return (NULL);
 	}
@@ -35,14 +36,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 
 	if (*head == NULL)
-	{  /* If the list is empty, the new node is the head */
+	{
 	*head = new_node;
 	}
 	else
 	{
 	last_node = *head;
-	while (last_node->next != NULL)  /* Traverse the list to find the last node */
+	while (last_node->next != NULL)
+	{
 	last_node = last_node->next;
+	}
 	last_node->next = new_node;
 	}
 
