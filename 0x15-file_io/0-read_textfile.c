514fdb3b4dbb4f7ca88d3c5b17bdb62755f6fdc8
keffakevin <kabagambekevin@gmai.com>
2023-05-09 17:52:43 +0200

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,30 +8,32 @@
  * @filename: the name of the file to read
  * @letters: the maximum number of letters to read and print
  *
- * Return: the actual number of letters read and printed, or 0 if an error occurred
+ * Return: the actual number of letters read and printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nread, nwritten = 0;
-    char buffer[1024];
+	int fd;
+	ssize_t nread, nwritten = 0;
+	char buffer[1024];
 
-    if (filename == NULL)
-        return 0;
+	if (filename == NULL)
+	return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
 
-    while (letters > 0 && (nread = read(fd, buffer, sizeof(buffer))) > 0) {
-        if (write(STDOUT_FILENO, buffer, nread) != nread) {
-            close(fd);
-            return 0;
-        }
-        nwritten += nread;
-        letters -= nread;
-    }
+	while (letters > 0 && (nread = read(fd, buffer, sizeof(buffer))) > 0)
+	{
+	if (write(STDOUT_FILENO, buffer, nread) != nread)
+	{
+	close(fd);
+	return (0);
+	}
+	nwritten += nread;
+	letters -= nread;
+	}
 
-    close(fd);
-    return nwritten;
+	close(fd);
+	return (nwritten);
 }
