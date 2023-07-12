3b104ba7afd984c93887e5db18bf89658f9c0987
keffakevin <kabagambekevin05@gamil.com>
2023-07-13 00:08:13 +0200

    unknown

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,34 +10,33 @@
  * @text_content: The NULL terminated string to add at the end of the file.
  *
  * Return: 1 on success, -1 on failure.
- *         If filename is NULL, return -1.
- *         If text_content is NULL, do not add anything to the file.
- *         Return 1 if the file exists and -1 if the file does not exist or if you
- *         do not have the required permissions to write the file.
+ * If filename is NULL, return -1.
+ * If text_content is NULL, do not add anything to the file.
+ * Return 1 if the file exists and -1 if the file does not exist
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    FILE *file;
-    int success = -1;
+	FILE *file;
+	int success = -1;
 
-    if (filename == NULL)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    file = fopen(filename, "a");
-    if (file == NULL)
-        return success;
+	file = fopen(filename, "a");
+	if (file == NULL)
+		return (success);
 
-    if (text_content != NULL)
-    {
-        if (fputs(text_content, file) >= 0)
-            success = 1;
-    }
-    else
-    {
-        success = 1;
-    }
+	if (text_content != NULL)
+	{
+		if (fputs(text_content, file) >= 0)
+			success = 1;
+	}
+	else
+	{
+		success = 1;
+	}
 
-    fclose(file);
-    return success;
+	fclose(file);
+	return (success);
 }
 
