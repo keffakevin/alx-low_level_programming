4d57b5a6ec9df565dc36a54263fcb1b691bfb55c
keffa <keffa@KVN>
2023-11-16 20:45:51 -0500

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function that print alahabetic
+ * @n:parameter description
+ * Return:1 and return 0 otherwise
+ */
+int _abs(int n)
+{
+if (n < 0)
+	n = -(n);
+else
+	n = (n);
+return (n);
+}
